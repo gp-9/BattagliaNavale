7b1941d23cd245bd1ab29fad1a1716be2ee829f4
trikkeballakke <tommaso.bolognesi@libero.it>
2023-01-12 14:36:12 +0100

    Implementation of submarine class

---
+++ include/Army/Submarine/Submarine.h
@@ -0,0 +1,11 @@
+#ifndef SUBMARINE_H_
+#define SUBMARINE_H_
+#include "../Army.h"
+
+class BattleShip::Submarine : public BattleShip::Army {
+    public:
+        Submarine(const BattleShip::point_t& center, const BattleShip::direction_t& direction, const BattleShip::Board& board);
+        bool makeAction(const BattleShip::point_t& origin, const BattleShip::point_t& target) override;
+};
+
+#endif // SUBMARINE_H_
